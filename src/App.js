5006cbab85d66f54ce0c50362e739498b72f04d8
bahram <bahram.karimi@bamilo.com>
2018-12-06 17:40:25 +0330

    Initial redux dependencies added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import firebase from 'firebase';
-import _ from 'lodash';
 
 //Import Components
 import Header from './components/Header';
@@ -23,70 +21,12 @@ class App extends Component {
     }
   }
 
-  componentWillMount() {
-    firebase.initializeApp({
-
-    });
-
-    firebase.database().ref('notes')
-      .on('value', snapshot => {
-        const fbstore = snapshot.val();
-        const store = _.map(fbstore, (value,id) => {
-          return {
-            id: id,
-            title: value.title,
-            details: value.details,
-          };
-        });
-
-        this.setState({
-          notes: store,
-        });
-
-      });
-  }
-
-  handleChange(event) {
-    const name = event.target.name;
-    const value = event.target.value;
-
-    this.setState({
-      [name]: value
-    });
-  }
-
-  handleSubmit(event){
-    console.log(`Your note: ${this.state.currentTitle} has been added!`);
-
-    const data = {
-      title: this.state.currentTitle,
-      details: this.state.currentDetails,
-    };
-
-    firebase.database().ref('/notes').push(data, response => response);
-
-    this.setState({
-      currentTitle: '',
-      currentDetails: '',
-    });
-
-    event.preventDefault();
-  }
-
-  deleteNote(id){
-    firebase.database().ref(`/notes/${id}`).remove();
-  }
-
   render() {
     return (
       <div className="App">
         <Header name="Bahram"/>
-        <Form
-          currentTitle={this.state.currentTitle}
-          currentDetails={this.state.currentDetails}
-          handleChange={this.handleChange.bind(this)}
-          handleSubmit={this.handleSubmit.bind(this)}/>
-        <Grid notes={this.state.notes} deleteNote={this.deleteNote.bind(this)}/>
+        <Form />
+        <Grid />
       </div>
     );
   }
